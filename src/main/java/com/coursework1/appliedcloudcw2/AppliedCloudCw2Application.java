498fe25c42cb5e489ab0c70f0bb40be70a8935bf
AndreasHiropedi <s2015345@ed.ac.uk>
2024-02-22 17:37:55 +0000

    project setup complete

---
+++ src/main/java/com/coursework1/appliedcloudcw2/AppliedCloudCw2Application.java
@@ -0,0 +1,13 @@
+package com.coursework1.appliedcloudcw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliedCloudCw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppliedCloudCw2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/coursework1/appliedcloudcw2/AppliedCloudCw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework1.appliedcloudcw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliedCloudCw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
